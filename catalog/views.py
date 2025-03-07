da47069df57871e02ab7fe4dd170793ac8b08ca3
volko <Toxa.Vol@yandex.ru>
2025-03-07 16:03:14 +0300

    hwork 23 done

--- catalog/views.py
+++ catalog/views.py
@@ -3,20 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
+    return render(request, "catalog/contacts.html")
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    return render(request, "catalog/home.html")
 
 
 def contacts(request):
     if request.method == "POST":
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        return HttpResponse(f"""Спасибо, {name}! 
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(
+            f"""Спасибо, {name}! 
         Мы получили ваше сообщение: ({message}).
-        Мы перезвоним вам по телефону: {phone}""")
-    return render(request, 'catalog/contacts.html')
+        Мы перезвоним вам по телефону: {phone}"""
+        )
+    return render(request, "catalog/contacts.html")
