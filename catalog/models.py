77061180b6168c1fa368eb9cdaf691c66e09aa30
volko <Toxa.Vol@yandex.ru>
2025-03-19 11:06:07 +0300

    hwork 24 done

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название", help_text="Введите название продукта")
     description = models.TextField(verbose_name="Описание")
-    image = models.ImageField(upload_to="catalog/media", blank=True, null=True, verbose_name="Изображение")
+    image = models.ImageField(upload_to="products", blank=True, null=True, verbose_name="Изображение", default='placeholder.png')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
     price = models.FloatField(verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
@@ -31,3 +31,17 @@ class Product(models.Model):
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["category", "name"]
+
+
+class Contact(models.Model):
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    individual_number = models.CharField(max_length=100, verbose_name="ИНН")
+    address = models.TextField(verbose_name="Адрес")
+
+    def __str__(self):
+        return f"{self.country} {self.address}"
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+        ordering = ["country", "address"]
